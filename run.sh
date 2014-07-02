cc3164aaefb3936ef1c9d662d1dbd5f1c47007bd
Mitar <mitar.git@tnode.com>
2014-07-02 15:13:01 -0700

    Configured which IP range we are trusting for resolving proxied IPs.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #/bin/bash -e
 
-docker run -d --name phppgadmin -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
+# TODO: We should not trust whole Docker IP range
+
+docker run -d --name phppgadmin -e SET_REAL_IP_FROM=172.17.42.1/16 -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
