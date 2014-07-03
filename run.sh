e446a8306f984943e6dd3a206a92f6cc00d6cfd5
Mitar <mitar.git@tnode.com>
2014-07-03 02:54:44 -0700

    Set hostname.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # TODO: We should not trust whole Docker IP range
 
-docker run -d --name phppgadmin -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.net -e REMOTES=mail.cloyne.net -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
+docker run -d --name phppgadmin -h phppgadmin.phppgadmin.server2.docker -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.net -e REMOTES=mail.cloyne.net -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
