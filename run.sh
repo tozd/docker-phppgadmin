c71ec1e0f09f8e70f663934dbe9186e6b8bda3d7
Mitar <mitar.git@tnode.com>
2014-07-06 20:31:46 -0700

    Prepare directories.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #/bin/bash -e
 
+mkdir -p /srv/var/log/phppgadmin
+
 # TODO: We should not trust whole Docker IP range
 
 docker run -d --name phppgadmin -h phppgadmin.phppgadmin.server2.docker -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.net -e REMOTES=mail.cloyne.net -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
