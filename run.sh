c2e45ba3dc62f51f0ebf1de61ddba679420493cc
Mitar <mitar.git@tnode.com>
2015-11-16 07:10:33 -0800

    Run always.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p /srv/var/log/phppgadmin
 
 # TODO: We should not trust whole Docker IP range
 
-docker run -d --name phppgadmin --hostname phppgadmin -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.org -e REMOTES=mail.cloyne.net -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
+docker run -d --restart=always --name phppgadmin --hostname phppgadmin -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.org -e REMOTES=mail.cloyne.net -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/phppgadmin:/var/log/nginx cloyne/phppgadmin
