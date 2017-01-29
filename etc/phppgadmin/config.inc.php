04471d45dfc37b9fd52c79676bdbadc42b9a04b4
Mitar <mitar.github@tnode.com>
2017-01-28 23:57:49 -0800

    Updated hostname.

--- etc/phppgadmin/config.inc.php
+++ etc/phppgadmin/config.inc.php
@@ -15,7 +15,7 @@
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
-	$conf['servers'][0]['host'] = 'pgsql.server2.docker';
+	$conf['servers'][0]['host'] = 'pgsql';
 
 	// Database port on server (5432 is the PostgreSQL default)
 	$conf['servers'][0]['port'] = 5432;
