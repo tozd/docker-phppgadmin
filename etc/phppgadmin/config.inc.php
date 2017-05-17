6c99cd3c08a87957a91ceef8575f9f59f082760c
Mitar <mitar.git@tnode.com>
2017-05-17 00:05:19 -0700

    Configuring host through environment variables.

--- etc/phppgadmin/config.inc.php
+++ etc/phppgadmin/config.inc.php
@@ -15,10 +15,10 @@
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
-	$conf['servers'][0]['host'] = 'pgsql';
+	$conf['servers'][0]['host'] = getenv('PHPPGADMIN_HOST', true) ?: 'pgsql';
 
 	// Database port on server (5432 is the PostgreSQL default)
-	$conf['servers'][0]['port'] = 5432;
+	$conf['servers'][0]['port'] = intval(getenv('PHPPGADMIN_PRT', true) ?: '5432');
 
 	// Database SSL mode
 	// Possible options: disable, allow, prefer, require
