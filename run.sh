5d8fec22352c2fbcf5702301c63fe217b49d458a
Mitar <mitar.git@tnode.com>
2017-05-16 17:07:45 -0700

    Updating run.sh.

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ mkdir -p /srv/var/log/phppgadmin
 docker run --detach=true --restart=always --name phppgadmin --hostname phppgadmin \
  --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/phppgadmin/ \
  --env SET_REAL_IP_FROM=172.17.0.0/16 --env ADMINADDR=root@cloyne.org --env REMOTES=mail.cloyne.net \
- --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/phppgadmin:/var/log/nginx \
- cloyne/phppgadmin
+ --volume /srv/var/log/phppgadmin:/var/log/nginx \
+ tozd/phppgadmin
