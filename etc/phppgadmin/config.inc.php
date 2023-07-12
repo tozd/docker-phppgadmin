6d1cd030b19e300fe6afe2d104364436ff768150
Mitar <mitar.git@tnode.com>
2023-07-13 01:19:17 +0200

    Update to latest version.

--- etc/phppgadmin/config.inc.php
+++ etc/phppgadmin/config.inc.php
@@ -18,13 +18,13 @@
 	$conf['servers'][0]['host'] = getenv('PHPPGADMIN_HOST', true) ?: 'pgsql';
 
 	// Database port on server (5432 is the PostgreSQL default)
-	$conf['servers'][0]['port'] = intval(getenv('PHPPGADMIN_PRT', true) ?: '5432');
+	$conf['servers'][0]['port'] = intval(getenv('PHPPGADMIN_PORT', true) ?: '5432');
 
 	// Database SSL mode
 	// Possible options: disable, allow, prefer, require
 	// To require SSL on older servers use option: legacy
 	// To ignore the SSL mode, use option: unspecified
-	$conf['servers'][0]['sslmode'] = 'require';
+	$conf['servers'][0]['sslmode'] = 'allow';
 
 	// Change the default database only if you cannot connect to template1.
 	// For a PostgreSQL 8.1+ server, you can set this to 'postgres'.
@@ -35,12 +35,6 @@
 	$conf['servers'][0]['pg_dump_path'] = '/usr/bin/pg_dump';
 	$conf['servers'][0]['pg_dumpall_path'] = '/usr/bin/pg_dumpall';
 
-	// Slony (www.slony.info) support?
-	$conf['servers'][0]['slony_support'] = false;
-	// Specify the path to the Slony SQL scripts (where slony1_base.sql is located, etc.)
-	// No trailing slash.
-	$conf['servers'][0]['slony_sql'] = '/usr/share/slony1';
-
 	// Example for a second server (PostgreSQL for Windows)
 	//$conf['servers'][1]['desc'] = 'Test Server';
 	//$conf['servers'][1]['host'] = '127.0.0.1';
@@ -49,37 +43,60 @@
 	//$conf['servers'][1]['defaultdb'] = 'template1';
 	//$conf['servers'][1]['pg_dump_path'] = 'C:\\Program Files\\PostgreSQL\\8.0\\bin\\pg_dump.exe';
 	//$conf['servers'][1]['pg_dumpall_path'] = 'C:\\Program Files\\PostgreSQL\\8.0\\bin\\pg_dumpall.exe';
-	//$conf['servers'][1]['slony_support'] = false;
-	//$conf['servers'][1]['slony_sql'] = 'C:\\Program Files\\PostgreSQL\\8.0\\share';
-	
-	
-	// Example of groups definition.
-	// Groups allow administrators to logicaly group servers together under group nodes in the left browser tree
-	//
-	// The group '0' description
+
+
+	/* Groups definition */
+	/* Groups allow administrators to logicaly group servers together under
+	 * group nodes in the left browser tree
+	 *
+	 * The group '0' description
+	 */
 	//$conf['srv_groups'][0]['desc'] = 'group one';
-	//
-	// Add here servers indexes belonging to the group '0' seperated by comma
-	//$conf['srv_groups'][0]['servers'] = '0,1,2'; 
-	//
-	// A server can belong to multi groups
+
+	/* Add here servers indexes belonging to the group '0' separated by comma */
+	//$conf['srv_groups'][0]['servers'] = '0,1,2';
+
+	/* A server can belong to multi groups. Here server 1 is referenced in both
+	 * 'group one' and 'group two'*/
 	//$conf['srv_groups'][1]['desc'] = 'group two';
 	//$conf['srv_groups'][1]['servers'] = '3,1';
-	
+
+	/* A group can be nested in one or more existing groups using the 'parents'
+	 * parameter. Here the group 'group three' contains only one server and will
+	 * appear as a subgroup in both 'group one' and 'group two':
+	 */
+	//$conf['srv_groups'][2]['desc'] = 'group three';
+	//$conf['srv_groups'][2]['servers'] = '4';
+	//$conf['srv_groups'][2]['parents'] = '0,1';
+
+	/* Warning: Only groups with no parents appears at the root of the tree. */
+
+	/* You can apply specific theme depending on servers, users and databases
+	 * The priority order is :
+	 *   * the theme defined for a server
+	 *   * the theme defined for a database apply over the server one
+	 *   * the theme defined for a user apply over the database one
+	 */
+	/* Example for servers */
+	//$conf['servers'][0]['theme']['default'] = 'default';
+	/* Example for users */
+	//$conf['servers'][0]['theme']['user']['specific_user'] = 'default';
+	/* Example for databases */
+	//$conf['servers'][0]['theme']['db']['specific_db'] = 'default';
 
 	// Default language. E.g.: 'english', 'polish', etc.  See lang/ directory
-	// for all possibilities. If you specify 'auto' (the default) it will use 
+	// for all possibilities. If you specify 'auto' (the default) it will use
 	// your browser preference.
 	$conf['default_lang'] = 'auto';
 
-	// AutoComplete uses AJAX interaction to list foreign key values 
-	// on insert fields. It currently only works on single column 
+	// AutoComplete uses AJAX interaction to list foreign key values
+	// on insert fields. It currently only works on single column
 	// foreign keys. You can choose one of the following values:
 	// 'default on' enables AutoComplete and turns it on by default.
 	// 'default off' enables AutoComplete but turns it off by default.
 	// 'disable' disables AutoComplete.
 	$conf['autocomplete'] = 'default on';
-	
+
 	// If extra login security is true, then logins via phpPgAdmin with no
 	// password or certain usernames (pgsql, postgres, root, administrator)
 	// will be denied. Only set this false once you have read the FAQ and
@@ -97,41 +114,28 @@
 	// a database, but they do take up space in the interface.
 	$conf['show_comments'] = true;
 
-	// Display "advanced" objects? Setting this to true will show 
-	// aggregates, types, operators, operator classes, conversions, 
-	// languages and casts in phpPgAdmin. These objects are rarely 
+	// Display "advanced" objects? Setting this to true will show
+	// aggregates, types, operators, operator classes, conversions,
+	// languages and casts in phpPgAdmin. These objects are rarely
 	// administered and can clutter the interface.
 	$conf['show_advanced'] = false;
 
 	// Display "system" objects?
 	$conf['show_system'] = false;
 
-	// Display reports feature?  For this feature to work, you must
-	// install the reports database as explained in the INSTALL file.
-	$conf['show_reports'] = false;
-
-	// Database and table for reports
-	$conf['reports_db'] = 'phppgadmin';
-	$conf['reports_schema'] = 'public';
-	$conf['reports_table'] = 'ppa_reports';
-
-	// Only show owned reports?
-	// Note: This does not prevent people from accessing other reports by
-	// other means.
-	$conf['owned_reports_only'] = false;
-
 	// Minimum length users can set their password to.
 	$conf['min_password_length'] = 7;
 
 	// Width of the left frame in pixels (object browser)
 	$conf['left_width'] = 200;
-	
+
 	// Which look & feel theme to use
 	$conf['theme'] = 'default';
-	
+
 	// Show OIDs when browsing tables?
+	// Only supported in versions <=11
 	$conf['show_oids'] = false;
-	
+
 	// Max rows to show on a page when browsing record sets
 	$conf['max_rows'] = 1000;
 
@@ -145,11 +149,21 @@
 	// '%s', if present, will be replaced with the PostgreSQL version
 	// (e.g. 8.4 )
 	$conf['help_base'] = 'http://www.postgresql.org/docs/%s/interactive/';
-	
+
 	// Configuration for ajax scripts
 	// Time in seconds. If set to 0, refreshing data using ajax will be disabled (locks and activity pages)
 	$conf['ajax_refresh'] = 3;
-	
+
+	/** Plugins management
+	 * Add plugin names to the following array to activate them
+	 * Example:
+	 *   $conf['plugins'] = array(
+	 *     'Example',
+	 *     'Slony'
+	 *   );
+	 */
+	$conf['plugins'] = array();
+
 	/*****************************************
 	 * Don't modify anything below this line *
 	 *****************************************/
--- latest.dockerfile
+++ latest.dockerfile
@@ -1,12 +1,12 @@
-FROM registry.gitlab.com/tozd/docker/php:5.5
+FROM registry.gitlab.com/tozd/docker/php:8.2
 
 ENV PHPPGADMIN_HOST pgsql
 ENV PHPPGADMIN_PORT 5432
 
 RUN apt-get update -q -q && \
- apt-get install phppgadmin --yes --force-yes && \
- apt-get install postgresql-client-9.3 --no-install-recommends --yes --force-yes && \
- sed -i 's/.*<h1>.*<\/h1>.*//' /usr/share/phppgadmin/intro.php && \
- apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* ~/.cache ~/.npm
+  apt-get install phppgadmin --yes --force-yes && \
+  apt-get install postgresql-client-14 --no-install-recommends --yes --force-yes && \
+  sed -i 's/.*<h1>.*<\/h1>.*//' /usr/share/phppgadmin/intro.php && \
+  apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* ~/.cache ~/.npm
 
 COPY ./etc /etc
